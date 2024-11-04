197a542e7dc8e9609ef6f284d448c14346ce3831
knokko <knokogator@hotmail.com>
2024-11-04 18:43:31 +0100

    Add BitCountStream

---
+++ src/main/java/com/github/knokko/bitser/io/BitCountStream.java
@@ -0,0 +1,21 @@
+package com.github.knokko.bitser.io;
+
+import java.io.ByteArrayOutputStream;
+
+public class BitCountStream extends BitOutputStream {
+
+    private int counter;
+
+    public BitCountStream() {
+        super(new ByteArrayOutputStream());
+    }
+
+    @Override
+    public void write(boolean value) {
+        counter += 1;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+}
