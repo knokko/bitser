635191617f11169eace0f0868f6a2e5d0c5e5a3a
knokko <knokogator@hotmail.com>
2024-11-04 18:43:04 +0100

    Add InvalidBitFieldException

---
+++ src/main/java/com/github/knokko/bitser/exceptions/InvalidBitFieldException.java
@@ -0,0 +1,8 @@
+package com.github.knokko.bitser.exceptions;
+
+public class InvalidBitFieldException extends RuntimeException {
+
+    public InvalidBitFieldException(String message) {
+        super(message);
+    }
+}
