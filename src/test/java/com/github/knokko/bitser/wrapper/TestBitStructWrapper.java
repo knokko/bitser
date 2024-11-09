baf81d9aa1b28c695e9b46a120bb3280d6d16200
knokko <knokogator@hotmail.com>
2024-11-09 14:24:37 +0100

    Cleaning up tests

--- src/test/java/com/github/knokko/bitser/wrapper/TestBitStructWrapper.java
+++ src/test/java/com/github/knokko/bitser/wrapper/TestBitStructWrapper.java
@@ -1,7 +1,9 @@
 package com.github.knokko.bitser.wrapper;
 
-import com.github.knokko.bitser.structs.Chain;
-import com.github.knokko.bitser.structs.WandCooldowns;
+import com.github.knokko.bitser.BitStruct;
+import com.github.knokko.bitser.field.BitField;
+import com.github.knokko.bitser.field.IntegerField;
+import com.github.knokko.bitser.field.StructField;
 import com.github.knokko.bitser.io.BitInputStream;
 import com.github.knokko.bitser.io.BitOutputStream;
 import com.github.knokko.bitser.serialize.BitserCache;
@@ -14,10 +16,27 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TestBitStructWrapper {
 
+    @BitStruct(backwardCompatible = false)
+    private static class WandCooldowns {
+
+        @BitField(ordering = 1)
+        @IntegerField(minValue = 1, expectUniform = true)
+        public int cooldown;
+
+        @BitField(ordering = 0)
+        @IntegerField(minValue = 1, expectUniform = true)
+        private int maxCharges = 3;
+
+        @BitField(ordering = 2)
+        @IntegerField(minValue = 0, expectUniform = true)
+        public Integer rechargeTime;
+    }
+
     @Test
     public void testWandCooldowns() throws IOException {
         WandCooldowns wandCooldowns = new WandCooldowns();
         wandCooldowns.cooldown = 5;
+        wandCooldowns.maxCharges = 4;
         wandCooldowns.rechargeTime = 20;
 
         BitserWrapper<WandCooldowns> wandCooldownsWrapper = BitserWrapper.wrap(WandCooldowns.class);
@@ -33,7 +52,37 @@ public class TestBitStructWrapper {
         bitInput.close();
 
         assertEquals(5, loadedCooldowns.cooldown);
+        assertEquals(4, loadedCooldowns.maxCharges);
         assertEquals(20, loadedCooldowns.rechargeTime);
+    }
+
+    @BitStruct(backwardCompatible = false)
+    private static class Chain {
+
+        @BitField(ordering = 0, optional = true)
+        @StructField
+        public Chain next;
+
+        @BitField(ordering = 1)
+        @StructField
+        public Properties properties;
+
+        @BitStruct(backwardCompatible = false)
+        public static class Properties {
+
+            @BitField(ordering = 0)
+            @IntegerField(expectUniform = false)
+            public int strength;
+
+            @SuppressWarnings("unused")
+            private Properties() {
+                this.strength = 0;
+            }
+
+            public Properties(int strength) {
+                this.strength = strength;
+            }
+        }
     }
 
     @Test
