5602b12ea0e74ffad3bc3553fc8a6c0d1dc73644
knokko <knokogator@hotmail.com>
2025-01-27 18:14:01 +0100

    wip: add ClassField

--- src/main/java/com/github/knokko/bitser/wrapper/StructFieldWrapper.java
+++ src/main/java/com/github/knokko/bitser/wrapper/StructFieldWrapper.java
@@ -1,5 +1,8 @@
 package com.github.knokko.bitser.wrapper;
 
+import com.github.knokko.bitser.exceptions.InvalidBitFieldException;
+import com.github.knokko.bitser.exceptions.InvalidBitValueException;
+import com.github.knokko.bitser.field.ClassField;
 import com.github.knokko.bitser.io.BitInputStream;
 import com.github.knokko.bitser.io.BitOutputStream;
 import com.github.knokko.bitser.serialize.BitserCache;
@@ -8,12 +11,33 @@ import com.github.knokko.bitser.util.ReferenceIdMapper;
 import com.github.knokko.bitser.util.VirtualField;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.Set;
+
+import static com.github.knokko.bitser.serialize.IntegerBitser.decodeUniformInteger;
+import static com.github.knokko.bitser.serialize.IntegerBitser.encodeUniformInteger;
 
 public class StructFieldWrapper extends BitFieldWrapper {
 
-	StructFieldWrapper(VirtualField field) {
+	private final Class<?>[] allowed;
+
+	StructFieldWrapper(VirtualField field, ClassField classField) {
 		super(field);
+		if (classField != null) {
+			try {
+				Field hierarchyField = classField.root().getDeclaredField("BITSER_HIERARCHY");
+				if (!Modifier.isPublic(hierarchyField.getModifiers())) hierarchyField.setAccessible(true);
+				allowed = (Class<?>[]) hierarchyField.get(null);
+			} catch (NoSuchFieldException e) {
+				throw new InvalidBitFieldException("Class " + classField.root() + " must have a constant named 'BITSER_HIERARCHY'");
+			} catch (ClassCastException wrongType) {
+				throw new InvalidBitFieldException("BITSER_HIERARCHY of " + classField.root() + " must be a Class<?>[]");
+			} catch (IllegalAccessException e) {
+				throw new InvalidBitFieldException("Can't make BITSER_HIERARCHY of " + classField.root() + " accessible");
+			}
+		} else this.allowed = new Class<?>[] { field.type };
 	}
 
 	@Override
@@ -22,26 +46,40 @@ public class StructFieldWrapper extends BitFieldWrapper {
 			Set<String> stableLabels, Set<String> unstableLabels, Set<Object> visitedObjects
 	) {
 		super.collectReferenceTargetLabels(cache, declaredTargetLabels, stableLabels, unstableLabels, visitedObjects);
-		cache.getWrapper(field.type).collectReferenceTargetLabels(
-				cache, declaredTargetLabels, stableLabels, unstableLabels, visitedObjects
-		);
+
+		for (Class<?> structClass : allowed) {
+			cache.getWrapper(structClass).collectReferenceTargetLabels(
+					cache, declaredTargetLabels, stableLabels, unstableLabels, visitedObjects
+			);
+		}
 	}
 
 	@Override
 	void registerReferenceTargets(Object value, BitserCache cache, ReferenceIdMapper idMapper) {
 		super.registerReferenceTargets(value, cache, idMapper);
-		if (value != null) cache.getWrapper(field.type).registerReferenceTargets(value, cache, idMapper);
+		if (value != null) cache.getWrapper(value.getClass()).registerReferenceTargets(value, cache, idMapper);
 	}
 
 	@Override
 	void writeValue(Object value, BitOutputStream output, BitserCache cache, ReferenceIdMapper idMapper) throws IOException {
-		cache.getWrapper(field.type).write(value, output, cache, idMapper);
+		for (int index = 0; index < allowed.length; index++) {
+			if (allowed[index] == value.getClass())	{
+				encodeUniformInteger(index, 0, allowed.length - 1, output);
+				cache.getWrapper(value.getClass()).write(value, output, cache, idMapper);
+				return;
+			}
+		}
+
+		throw new InvalidBitValueException(
+				"Struct class " + value.getClass() + " is not in " + Arrays.toString(allowed)
+		);
 	}
 
 	@Override
 	void readValue(
 			BitInputStream input, BitserCache cache, ReferenceIdLoader idLoader, ValueConsumer setValue
 	) throws IOException {
-		cache.getWrapper(field.type).read(input, cache, idLoader, setValue);
+		int index = (int) decodeUniformInteger(0, allowed.length - 1, input);
+		cache.getWrapper(allowed[index]).read(input, cache, idLoader, setValue);
 	}
 }
